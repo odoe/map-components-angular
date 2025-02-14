b1f27ddee75f8bb4e275874b5d71c16f7f883944
Rene Rubalcava <rrubalcava@esri.com>
2025-02-14 08:49:45 -0800

    chore: init

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import type { OnInit } from "@angular/core";
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ElementRef,
+  viewChild,
+} from "@angular/core";
+
+import { setAssetPath } from "@esri/calcite-components/dist/components";
+
+import "@arcgis/core/assets/esri/themes/dark/main.css";
+
+import "@arcgis/map-components/dist/components/arcgis-map";
+import "@arcgis/map-components/dist/components/arcgis-zoom";
+import "@arcgis/map-components/dist/components/arcgis-search";
+import "@arcgis/map-components/dist/components/arcgis-legend";
+
+setAssetPath(
+  "https://cdn.jsdelivr.net/npm/@esri/calcite-components@3.0.0-next.120/dist/calcite/assets",
+);
+
+@Component({
+  selector: "app-root",
+  standalone: true,
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class AppComponent implements OnInit {
+  title = "map-components-angular-sample";
+
+  myMap = viewChild<ElementRef<HTMLArcgisMapElement>>("myMap");
+
+  arcgisViewReadyChange(event: HTMLArcgisMapElement["arcgisViewReadyChange"]) {
+    console.log("View ready", this.myMap()?.nativeElement);
+    console.log("MapView ready", event);
+  }
+
+  ngOnInit() {
+    console.log("OnInit");
+  }
+}
